d2af9bfc140fe708536aeff7429c6c1814ce9b14
sayligadgil <sayligadgil06@gmail.com>
2025-08-20 12:22:53 +0530

    greater2.py

---
+++ greater2.py
@@ -0,0 +1,7 @@
+x=int(input("Enter an integer: "))
+y=int(input("Enter another integer: "))
+if x > y:
+    print(f"{x} is greater than {y}")
+else:
+    print(f"{y} is greater than or equal to {x}")
+    
