3d85c3c21cd70428447dd97992a64d99b2898e30
sayligadgil <sayligadgil06@gmail.com>
2025-08-24 09:01:43 +0530

    mergeandsort.py

---
+++ mergeandsort.py
@@ -0,0 +1,8 @@
+oneList = [2,4,6,8,10]
+twoList = [1,3,5,7,9]
+mergedList = oneList + twoList
+print(mergedList)
+##OUTPUT:[2, 4, 6, 8, 10, 1, 3, 5, 7, 9]
+sortedList = sorted(mergedList)
+print(sortedList)
+##OUTPUT:[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
