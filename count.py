54e6d874bd02ba7cbc7dae46ccc4127ef6ec3876
sayligadgil <sayligadgil06@gmail.com>
2025-08-24 09:04:09 +0530

    count.py

---
+++ count.py
@@ -0,0 +1,10 @@
+oneList = [2,4,5,5,6,6,7,8,9]
+oneList.count(5)
+print(oneList.count(5))
+##OUTPUT:2
+oneList.count(10)
+print(oneList.count(10))
+##OUTPUT:0
+oneList.count(2)
+print(oneList.count(2))
+##OUTPUT:1
