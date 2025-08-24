d77b499951c905e794c74b4aefed15d2521a9ed4
sayligadgil <sayligadgil06@gmail.com>
2025-08-24 09:09:12 +0530

    collectiontolist.py

---
+++ collectiontolist.py
@@ -0,0 +1,14 @@
+# Convert a tuple to a list
+my_tuple = (1, 2, 3)
+my_list = list(my_tuple)
+
+# Convert a set to a list
+my_set = {4, 5, 6}
+my_list2 = list(my_set)
+
+# Convert dictionary keys to a list
+my_dict = {'a': 1, 'b': 2}
+keys_list = list(my_dict.keys())
+
+# Convert dictionary values to a list
+values_list = list(my_dict.values())
